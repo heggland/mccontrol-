df453d7ad42fbd25bd1630f00beadb4615d7b56b
heggland <kjetilheggland@gmail.com>
2025-01-02 16:47:54 +0100

    fix multiple players can interact with blocks

--- src/main/java/org/hegglandtech/mccontrol/utils/BlockPlaceBreak.java
+++ src/main/java/org/hegglandtech/mccontrol/utils/BlockPlaceBreak.java
@@ -10,30 +10,30 @@ public class BlockPlaceBreak {
 
     private final MemoryStorage memoryStorage;
 
-
     public BlockPlaceBreak() {
         this.memoryStorage = Mccontrol.getInstance().getMemoryStorage();
     }
 
     public boolean validate(Player player) {
-
         List<String> memory = memoryStorage.getMemory();
 
+        System.out.println("Player interacted with block: " + player.getUniqueId());
+        System.out.println("Memory: " + memory);
+
         if (memory == null || memory.isEmpty()) {
+            return true;
+        }
+
+        String playerEntry = memory.stream()
+                .filter(line -> line.contains(player.getUniqueId().toString()))
+                .findFirst()
+                .orElse(null);
+
+        if (playerEntry != null) {
+            System.out.println("Player is in memory: " + playerEntry);
+            return McPlayer.canInteractWithBlocks(playerEntry);
+        } else {
             return false;
         }
-
-        for (String line : memory) {
-            if (line.contains(player.getUniqueId().toString())) {
-
-                if (!McPlayer.canInteractWithBlocks(line)) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
