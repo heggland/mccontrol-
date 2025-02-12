2b3ef5978c7819b540c09a5a79246dd89bb966a8
heggland <kjetilheggland@gmail.com>
2025-02-12 18:59:55 +0100

    Update PlayerUpdatePermission.java

--- src/main/java/org/hegglandtech/mccontrol/utils/PlayerUpdatePermission.java
+++ src/main/java/org/hegglandtech/mccontrol/utils/PlayerUpdatePermission.java
@@ -21,7 +21,8 @@ public class PlayerUpdatePermission {
         org.bukkit.entity.Player player = Mccontrol.getInstance().getServer().getPlayer(playerName);
 
         if (player == null) {
-            Mccontrol.getInstance().getLogger().warning("Player " + playerName + " not found.");
+            ServerLogger.print("Online player " + playerName + " not found.");
+            ServerLogger.print("If UUID is used, trying to find player by UUID.");
             updatePlayerUsingUuid(permission, action, playerName);
             return;
         }
@@ -50,6 +51,7 @@ public class PlayerUpdatePermission {
         Player playerData = loadPlayerFromMemory.getPlayerByPlayerUuid(uuid);
 
         if (playerData == null) {
+            ServerLogger.print("Player with UUID " + uuid + " not found.");
             return;
         }
 
@@ -60,6 +62,8 @@ public class PlayerUpdatePermission {
         }
 
         save(playerData);
+
+        ServerLogger.print(playerData.name + " has been " + action + "ed the permission " + permission + ". Command issued by " + this.player.getName());
     }
 
     private static void save(Player playerData) {
